f817c42f8a6e2c5e747235ce7a1ba83b2da30da2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-09-05 14:12:29 +0530

    fix:decorator in body

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { Request } from "express";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -6,7 +7,10 @@ export class AuthController{
     constructor(private authService:AuthService) {}
 
     @Post('signup')
-    signup() {
+    signup(@Body() dto:any) {
+        console.log({
+            dto,
+        })
         return this.authService.signup()
      }
 
