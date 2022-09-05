b1b6ea6e3c738be301f3e464305407b04e4c41a4
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-09-05 16:56:36 +0530

    fix:singup routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { Authdto } from "./dto";
+import * as argon from "argon2"
+import { Prisma } from "@prisma/client";
 @Injectable({})
 export class AuthService {
   constructor(private prisma:PrismaService){}
-    signup(dto:Authdto) {
-        return {msg: 'I have signed up'};
+    async signup(dto:Authdto) {
+
+        //generate the has the password
+
+        const hash=await argon.hash(dto.password);
+
+        //store the user details from database
+
+        const user=await this.prisma.user.create({
+            data:{ 
+              email: dto.email,
+              hash,
+            },
+        });
+
+
+        //given to response in saved data 
+        return user
     }
 
     signin(){
